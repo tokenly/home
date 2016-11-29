a6c5651f6281b281d920d6f1402640d334bc363c
Adam Raider <adamjraider@gmail.com>
2016-11-29 17:46:06 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Tokenly</h1>
+    <h2>Ownership Reinvented</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass?indentedSyntax=true">
+@import 'sass/application.sass'
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var path = require('path')
+var webpack = require('webpack')
+var autoprefixer = require('autoprefixer')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax=1',
+          scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
+          pug: 'pug-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      vue: {
+        // use custom postcss plugins
+        postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
