940bc896391c890c9234abff0e1a55a93cf40ddf
randolrs <r.shane.randolph@gmail.com>
2017-11-23 15:39:05 -0500

    add team bios component

---
+++ src/components/team-member.vue
@@ -0,0 +1,101 @@
+<template lang="pug">
+div.team-member
+  div.team-member__image
+    div(
+      v-bind:style="imageBackground"
+    )
+  div.team-member__content
+    p.name
+      span {{ this.name }}
+    p.title
+      span {{ this.title }}
+    p.summary
+      span {{ this.summary }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+    elID: {
+      'type': String
+    },
+    name: {
+      'type': String
+    },
+    title: {
+      'type': String
+    },
+    summary: {
+      'type': String
+    },
+    imgURL: {
+      'type': String
+    }
+  },
+  created () {
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+    imageBackground () {
+      let style = {
+        backgroundImage: 'url(' + this.imgURL + ')'
+      }
+      return style
+    }
+  }
+}
+</script>
+
+<style lang="sass?indentedSyntax=true" scoped>
+
+.team-member
+  padding: 10px
+  text-align: left
+  &__image, &__content
+    padding: 10px
+    vertical-align: middle
+  &__content
+    padding: 10px
+    vertical-align: middle
+    p
+      line-height: 1
+    p.name
+      font-size: 18px
+      color: #333
+      font-weight: 600
+      margin-bottom: 5px
+    p.title
+      color: #999
+      margin-bottom: 25px
+    p.summary
+      color: #111
+      line-height: 1.5
+
+.team-member__image
+  width: 100px
+  height: 100px
+  div
+    border-radius: 50%
+    max-width: 100%
+    height: 100%
+    background-size: cover
+    background-repeat: no-repeat
+    background-position: 50% 50%
+
+@media(max-width: 767px)
+  .product__row
+    &__image
+      display: none
+    &__content
+      width: 100%
+
+</style>
--- src/components/team.vue
+++ src/components/team.vue
@@ -5,14 +5,64 @@ section.team
       header
         span Our Team
       p
-        span Our 9 person team includes blockchain and finance experts together with veteran software engineers. Here are our four founders:
+        span Our 9 person team includes blockchain and finance experts together with veteran software engineers. Here are five key team members:
+      div.team-member-content
+        div.team-member-content__entry(v-for="(teamMember, index) in this.teamData")
+          TeamMember(
+            :elID = "teamMember.elID"
+            :name = "teamMember.name"
+            :title = "teamMember.title"
+            :summary = "teamMember.summary"
+            :imgURL = "teamMember.imgURL"
+          )
 
 </template>
 
 <script>
+
+import TeamMember from './team-member.vue'
+
 export default {
+  components: {TeamMember},
   data () {
     return {
+      teamData: [
+        {
+          name: 'Adam Levine',
+          elID: 'adam-levine',
+          title: 'Founder, CEO',
+          summary: 'Founder of the "Lets Talk Bitcoin" network (LTBN), veteran project advisor in blockchain and token technology.',
+          imgURL: 'https://tekk.tokenly.com/wp-content/uploads/2017/02/100kbbio-e1493910044698-500x500.jpg'
+        },
+        {
+          name: 'Devon Weller',
+          elID: 'devon-weller',
+          title: 'Founder, CTO',
+          summary: '18-year vetern internet software application developer for companies such as Price Waterhouse Coopers and Booz Allen Hamilton. Counterparty protocol developer.',
+          imgURL: 'https://tekk.tokenly.com/wp-content/uploads/2017/02/devon-2013-nov-600-500x500.jpg'
+        },
+        {
+          name: 'Martin Rerak',
+          elID: 'martin-rerak',
+          title: "CSO",
+          summary: 'Experienced finance and strategic planning professional. Early blockchain evangelist.',
+          imgURL: 'https://tekk.tokenly.com/wp-content/uploads/2017/02/martin-rerak-500x500.jpg'
+        },
+        {
+          name: 'Steven Levine',
+          elID: 'steven-levine',
+          title: 'Founder, CFO',
+          summary: 'Founder of the "Lets Talk Bitcoin" network (LTBN),. Experienced innovator and entrepreneur. Grew prior startup to $12.9MM annual revenue.',
+          imgURL: 'https://tekk.tokenly.com/wp-content/uploads/2017/02/steve-levine-e1494026781173-500x500.jpg'
+        },
+        {
+          name: 'Nick Rathman',
+          elID: 'nick-rathman',
+          title: 'Founder, Senior Developer',
+          summary: 'Experienced backend developer with expertise in blockchain and Counterparty protocol.',
+          imgURL: 'https://tekk.tokenly.com/wp-content/uploads/2017/02/2-e1493910666963-500x500.jpg'
+        }
+      ]
     }
   },
   created () {
@@ -46,7 +96,15 @@ export default {
         font-weight: 700
       p
         text-align: left
-        color: #666
+        color: #888
+        font-size: 20px
         max-width: 600px
+        margin-bottom: 30px
 
+.team-member-content
+  text-align: left
+.team-member-content__entry
+  display: inline-block
+  width: 33.3%
+  vertical-align: top
 </style>
