789a027577889a4a556f017c0bca8455dc72824f
randolrs <r.shane.randolph@gmail.com>
2018-01-08 18:08:19 -0500

    push new home to root path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '../components/home.vue'
 import NewHome from '../components/new-home.vue'
 
 Vue.use(Router)
@@ -10,11 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/new-home',
       name: 'New Home',
       component: NewHome
     }
